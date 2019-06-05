3aa31cb7cfd6cd82138656ba0869b5f31c18bc57
jared <ace020771@gmail.com>
2019-06-06 00:02:18 +0800

    Add submodules.

---
+++ CMakeHomework/ext/fmt/CMakeFiles/fmt.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jared/Desktop/Junior-2/PROGRAMMING_LANGUAGE/HW/programming-language/CMakeHomework/ext/fmt/src/format.cc" "/home/jared/Desktop/Junior-2/PROGRAMMING_LANGUAGE/HW/programming-language/CMakeHomework/ext/fmt/CMakeFiles/fmt.dir/src/format.cc.o"
+  "/home/jared/Desktop/Junior-2/PROGRAMMING_LANGUAGE/HW/programming-language/CMakeHomework/ext/fmt/src/posix.cc" "/home/jared/Desktop/Junior-2/PROGRAMMING_LANGUAGE/HW/programming-language/CMakeHomework/ext/fmt/CMakeFiles/fmt.dir/src/posix.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "ext/fmt/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
