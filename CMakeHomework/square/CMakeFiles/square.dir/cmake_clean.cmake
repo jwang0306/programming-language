a68296ba3cd4c4b38afe8dc033688597b600e5f6
jared <ace020771@gmail.com>
2019-06-05 23:28:31 +0800

    Add square library.

---
+++ CMakeHomework/square/CMakeFiles/square.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/square.dir/square.cpp.o"
+  "libsquare.pdb"
+  "libsquare.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/square.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeHomework/square/square.cpp
@@ -0,0 +1,3 @@
+int Square(int x) {
+    return x*x;
+}
